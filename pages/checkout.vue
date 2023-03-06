7c74100c92ce79c5d7bb29029d7ca3993e087dd4
jldevcode <125075073+jldevcode@users.noreply.github.com>
2023-03-06 17:15:41 -0500

    Update checkout.vue

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -120,8 +120,26 @@ const payNow = async () => {
               <StripeElement ref="card" :elements="elements" :options="cardOptions" />
             </StripeElements>
           </Transition>
-        </div>
-
+        </div>        
+        <div class="mt-2 col-span-full">
+<form>
+        <script
+            src="https://checkout.epayco.co/checkout.js"
+            class="epayco-button"
+            data-epayco-key="491d6a0b6e992cf924edd8d3d088aff1"
+            data-epayco-amount="50000"
+            data-epayco-name="Vestido Mujer Primavera"
+            data-epayco-description="Vestido Mujer Primavera"
+            data-epayco-currency="cop"
+            data-epayco-country="co"
+            data-epayco-test="true"
+            data-epayco-external="false"
+            data-epayco-response="https://ejemplo.com/respuesta.html"
+            data-epayco-confirmation="https://ejemplo.com/confirmacion"
+            data-epayco-methodconfirmation="get">
+        </script>
+    </form>
+      <div>
         <div>
           <h2 class="font-semibold text-xl mb-4">Order Note (Optional)</h2>
           <textarea
